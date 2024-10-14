4fad3dc15101284146b56428d732b51c3d877cca
Lahiru-Avishka <lahirumadushanka82887721@gmail.com>
2024-10-14 22:50:29 +0530

    js

---
+++ app.js
@@ -0,0 +1,30 @@
+// console.log("Hello world");
+
+// let isEqual = (5 =='5');
+// let isThisEqual =(5 === '5');
+
+// console.log(isEqual);
+// console.log(isThisEqual);
+
+// ==object==
+// const customer={
+//     name:"Lahiru" ,
+//     age:22 ,
+//     email: "avi.com"
+// }
+// console.log(customer.name);
+// console.log(customer.age);
+// console.log(customer.email);
+
+//==Array==
+
+// const numbers = [1,2,3,4,5];
+// console.log(numbers);
+// console.log(numbers[0]);
+
+//==Loop==
+
+const numbers = [1,2,3,4,5,6];
+for(let i=0;i<numbers.length;i++){
+    console.log([i]);
+}
